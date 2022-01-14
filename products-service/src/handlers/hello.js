13e17e697c7f8513cf3bd9f2593b32ecb7c65211
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-01-14 09:16:16 -0300

    :recycle: update hello message lambda

--- products-service/src/handlers/hello.js
+++ products-service/src/handlers/hello.js
@@ -1,7 +1,7 @@
 async function hello(event, context) {
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from https://codingly.io' }),
+    body: JSON.stringify({ message: 'Hello from' }),
   };
 }
 
