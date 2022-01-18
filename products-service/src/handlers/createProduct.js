437d1534aa2ff093a0e4aae66cbc01593a514e64
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-01-18 09:09:00 -0300

    :sparkles: add dynamo db put to save product

--- products-service/src/handlers/createProduct.js
+++ products-service/src/handlers/createProduct.js
@@ -1,13 +1,25 @@
+import { v4 as uuid } from 'uuid'
+import AWS from 'aws-sdk'
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient()
+
 async function createProduct(event, context) {
   const { title, qtd, status } = JSON.parse(event.body);
   const now = new Date()
 
   const product = {
+    id: uuid,
     title,
     qtd,
     status,
     createdAt: now.toISOString()
   }
+
+  dynamoDb.put({
+    TableName: 'ProductsTable',
+    Item: product
+  }).promise()
+
   return {
     statusCode: 201,
     body: JSON.stringify(product),
