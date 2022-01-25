056c4294333a51113dc15f193e6e50c8810de60f
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-01-25 08:50:30 -0300

    :sparkles: use middy and http errors

--- products-service/src/handlers/createProduct.js
+++ products-service/src/handlers/createProduct.js
@@ -1,10 +1,15 @@
 import { v4 as uuid } from 'uuid'
 import AWS from 'aws-sdk'
+import middy from '@middy/core'
+import httpJsonBodyParser from '@middy/http-json-body-parser'
+import httpEventNormalizer from '@middy/http-event-normalizer'
+import httpErrorHendler from '@middy/http-error-handler'
+import createError from 'http-errors'
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient()
 
 async function createProduct(event, context) {
-  const { title, qtd, status } = JSON.parse(event.body);
+  const { title, qtd, status } = event.body;
   const now = new Date()
 
   const product = {
@@ -15,17 +20,26 @@ async function createProduct(event, context) {
     createdAt: now.toISOString()
   }
 
-  dynamoDb.put({
-    TableName: proccess.env.PRODUCTS_TABLE_NAME,
-    Item: product
-  }).promise()
-
+  try {
+    await dynamoDb.put({
+      TableName: proccess.env.PRODUCTS_TABLE_NAME,
+      Item: product
+    }).promise()
+  
+  } catch (error) {
+    console.log(error)
+    throw new createError.InternalServerError(error)
+  }
+ 
   return {
     statusCode: 201,
     body: JSON.stringify(product),
   };
 }
 
-export const handler = createProduct;
+export const handler = middy(createProduct)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHendler())
 
 
