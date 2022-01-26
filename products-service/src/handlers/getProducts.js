4d7a6e0e8e14b407d6be138ea04498dc8a7dd36f
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-01-26 08:41:01 -0300

    :art: use common middleware in get products to avoid dry

--- products-service/src/handlers/getProducts.js
+++ products-service/src/handlers/getProducts.js
@@ -1,8 +1,5 @@
 import AWS from 'aws-sdk'
-import middy from '@middy/core'
-import httpJsonBodyParser from '@middy/http-json-body-parser'
-import httpEventNormalizer from '@middy/http-event-normalizer'
-import httpErrorHendler from '@middy/http-error-handler'
+import commonMiddleware from '../../lib/commonMiddleware'
 import createError from 'http-errors'
 
 const dynamoDb = new AWS.DynamoDB.DocumentClient()
@@ -26,9 +23,6 @@ async function getProducts(event, context) {
   };
 }
 
-export const handler = middy(getProducts)
-  .use(httpJsonBodyParser())
-  .use(httpEventNormalizer())
-  .use(httpErrorHendler())
+export const handler = commonMiddleware(getProducts)
 
 
