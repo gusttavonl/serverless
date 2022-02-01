3e104f9b20c84ced524095979f3a0b04ba9e8d6a
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-02-01 09:04:07 -0300

    :sparkles: add create error and close product in process product

--- products-service/src/handlers/processProducts.js
+++ products-service/src/handlers/processProducts.js
@@ -1,8 +1,17 @@
+import createError from 'http-errors'
 import { getEndedProducts } from '../lib/getEndedProducts'
+import { closeProduct } from '../lib/closeProduct'
 
 async function processProducts(event, context) {
-  const productsToClose = await getEndedProducts()
-  console.log(productsToClose)
+  try {
+    const productsToClose = await getEndedProducts()
+    const productsClosed = productsToClose.map((product) => closeProduct(product))
+    await Promise.all(productsClosed)
+
+    return { closed: productsClosed.length}
+  } catch (error) {
+    throw new createError.InternalServerError(error)
+  }
 }
 
 export const handler = processProducts
