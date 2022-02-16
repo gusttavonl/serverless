b9df58c170d54267c6d78a78fc532329355dbe09
GustavoNoronha0 <gustavonoronha0@hotmail.com>
2022-02-16 08:38:33 -0300

    :sparkles: add test email in send email

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -1,7 +1,32 @@
+import AWS from 'aws-sdk';
+
+const ses = new AWS.SES({ region: 'sa-east-1' });
+
 async function sendMail(event, context) {
-  return event
+  const params = {
+    Source: 'gustavonoronha0@hotmail.com',
+    Destination: {
+      ToAddresses: ['gustavonoronha0@hotmail.com'],
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: 'Hello from Gustavo Noronha!',
+        },
+      },
+      Subject: {
+        Data: 'Test Mail',
+      },
+    },
+  };
+
+  try {
+    const result = await ses.sendEmail(params).promise();
+    console.log(result);
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export const handler = sendMail;
-
-
